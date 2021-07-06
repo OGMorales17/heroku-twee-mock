7ecfae457929edde2b98b8d03ce1a3d19c5da52f
Orlando G Morales <orlandomorales@Orlandos-MacBook-Pro.local>
2021-07-05 20:25:32 -0400

    change the uri

--- app.py
+++ app.py
@@ -4,10 +4,15 @@ from models import connect_db, db, User, Tweet
 from forms import UserForm, TweetForm
 from sqlalchemy.exc import IntegrityError
 import os
+import re
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    "DATABASE_URL", "postgresql:///auth_demo")
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+#     "DATABASE_URL", "postgresql:///auth_demo")
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "hellosecret1")
