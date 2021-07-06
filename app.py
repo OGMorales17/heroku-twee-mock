1292e317aca2f170aad471e30fd7c56f00dff8c1
Orlando G Morales <orlandomorales@Orlandos-MacBook-Pro.local>
2021-07-06 01:24:10 -0400

    change the psql

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
 #     "DATABASE_URL", "postgresql:///auth_demo")
 
-uri = os.getenv["DATABASE_URL"]  # or other relevant config var
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
